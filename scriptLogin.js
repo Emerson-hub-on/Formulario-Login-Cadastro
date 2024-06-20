ec9fcdf65cb4609e2e3cd5b834ab20f7ce2eb8b1
Emerson-hub-on <sousaemerson.bolgmail.com>
2024-06-20 12:00:44 -0300

    Commit git

---
+++ scriptLogin.js
@@ -0,0 +1,65 @@
+
+let Loginvalidator = {
+    handleSubmit: (event) => {     //Função que para o evento enquanto tiver um submit
+        event.preventDefault();
+        let send = true;
+        let inputs = form.querySelectorAll('input');
+
+        Loginvalidator.clearErrors();
+
+        for (let i = 0; i < inputs.length; i++) {
+            let input = inputs[i];
+            console.log(input);
+            let check = Loginvalidator.checkInput(input);
+            if (check !== true) {
+                send = false;
+                Loginvalidator.showError(input, check);
+            }
+        }
+        if (send) {
+            form.submit();
+        }
+    },
+    checkInput: (input) => {
+        let rules = input.getAttribute('data-rules');
+
+        if (rules !== null) {
+            rules = rules.split('|');
+            for (let k in rules) {
+                let rDetails = rules[k].split('=');
+                switch (rDetails[0]) {
+                    case 'required':
+                        if (input.value == '') {
+                            return 'Campo não pode ser vazio';
+                        }
+                        break;
+                    case 'min':
+                        if (input.value.length < rDetails[1]) {
+                            return 'Campo tem que ter pelo menos ' + rDetails[1] + ' caracteres';
+                        }
+                        break;
+                }
+            }
+        }
+        return true;
+    },
+    showError: (input, error) => {
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErrors: () => {
+        let inputs = form.querySelectorAll('input')
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].style = '';
+        }
+        let errorElement = document.querySelectorAll('.error');
+        for (let i = 0; i < errorElement.length; i++) {
+            errorElement[i].remove();
+        }
+    }
+}
+let form = document.querySelector('.loginvalidator'); //Pegar o formulário que utilizaremos no código
+form.addEventListener('submit', Loginvalidator.handleSubmit); //Monitoramento para validar quando tiver um 'submit'
+
